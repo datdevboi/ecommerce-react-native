94856d4f6cc552ce0645e9a6fdc7486d64b15cdc
camilo17 <cami172013@gmail.com>
2018-02-22 22:02:05 -0600

    Gave signup more functionality and added some simple styling to make it feel more natural.

--- client/src/routes/Signup.js
+++ client/src/routes/Signup.js
@@ -1,4 +1,72 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TextInput, StyleSheet, Button} from 'react-native';
 
-export default () => (<Text>Hello World</Text>)
+export default class Signup extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            values: {
+                name: '',
+                username: '',
+                password: ''
+            },
+            errors: {},
+            isSubmitting: false
+            
+        }
+
+        this.handleTextChange = this.handleTextChange.bind(this);
+
+        
+    }
+
+    onSubmit() {
+        console.log('Form Submitted');
+    }
+
+    handleTextChange(key, value) {
+        this.setState(state => ({
+            values: {
+                ...state.values,
+                [key]: value
+
+            }
+        }))
+
+        
+    }
+    
+
+
+    render() {
+        const {values: {name, username, password}} = this.state;
+
+
+        return (
+            <View style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <View style={{ width: 200 }}>
+                    <TextInput onChangeText={(text) => this.handleTextChange('name', text)} value={name} style={styles.field} placeholder="name"/>
+                    <TextInput onChangeText={(text) => this.handleTextChange('username', text)}value={username} style={styles.field} placeholder="email"/>
+                    <TextInput onChangeText={(text) => this.handleTextChange('password', text)}value={password} style={styles.field} placeholder="password"/>
+                    <Button  style={styles.button} color="purple" title="Sign Up" onPress={this.onSubmit}/>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    field: {
+        borderBottomWidth: 1,
+        fontSize: 20,
+        marginBottom: 15
+    },
+    button: {
+        backgroundColor: '#000000',
+        height: 50,
+        width: 150
+    }
+})
+
+
